c3da3420029ef1a757ac574d7d5a95cd98477140
ycj0808 <ycj0808@126.com>
2016-07-12 17:59:05 +0800

    增加知乎日报的访问接口

--- app/src/main/java/com/android/ice/zhihudaily/mvp/view/DownloadView.java
+++ app/src/main/java/com/android/ice/zhihudaily/mvp/view/DownloadView.java
@@ -3,10 +3,12 @@ package com.android.ice.zhihudaily.mvp.view;
 import com.hannesdorfmann.mosby.mvp.MvpView;
 
 /**
+ * 下载
  * Created by yangchj on 2016/7/12 0012.
  * email:yangchj@neusoft.com
  */
 public interface DownloadView extends MvpView{
-    public void start();
+    public void startDownload();
     public void setProgress(int progress);
+    public void endDownload();
 }
