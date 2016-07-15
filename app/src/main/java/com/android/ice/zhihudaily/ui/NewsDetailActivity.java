baf1a741dac07be9354f8e2ef939eca63a8daa69
ycj0808 <ycj0808@126.com>
2016-07-15 10:58:21 +0800

    增加知乎日报的访问接口

--- app/src/main/java/com/android/ice/zhihudaily/ui/NewsDetailActivity.java
+++ app/src/main/java/com/android/ice/zhihudaily/ui/NewsDetailActivity.java
@@ -2,7 +2,6 @@ package com.android.ice.zhihudaily.ui;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-
 import com.android.ice.zhihudaily.R;
 import com.android.ice.zhihudaily.mvp.base.activity.BaseSwipeBackMvpActivity;
 import com.android.ice.zhihudaily.mvp.presenter.NewsDetailRxPresenter;
