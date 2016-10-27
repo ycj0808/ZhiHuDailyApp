37444e34a96a7da134acc810f6960cf864966461
ycj0808 <ycj0808@126.com>
2016-10-27 14:18:57 +0800

    图片加载工厂类的构建

--- app/src/main/java/com/android/ice/zhihudaily/mvp/base/fragment/BaseMvpFragment.java
+++ app/src/main/java/com/android/ice/zhihudaily/mvp/base/fragment/BaseMvpFragment.java
@@ -1,20 +1,36 @@
 package com.android.ice.zhihudaily.mvp.base.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.hannesdorfmann.mosby.mvp.MvpFragment;
 import com.hannesdorfmann.mosby.mvp.MvpPresenter;
 import com.hannesdorfmann.mosby.mvp.MvpView;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by yangchj on 2016/7/8 0008.
  * email:yangchj@neusoft.com
  */
 public abstract class BaseMvpFragment<V extends MvpView, P extends MvpPresenter<V>> extends MvpFragment<V,P> {
+    protected View mRootView;
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mRootView= inflater.inflate(getLayoutId(), container, false);
+        return mRootView;
     }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+        afterCreate(savedInstanceState);
+    }
+    protected abstract int getLayoutId();
+    protected abstract void afterCreate(Bundle savedInstanceState);
 }
